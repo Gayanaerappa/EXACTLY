e0559765dbb5ed405bd1f1009cebd9c71052d791
Gayanaerappa <gayanaerappa2027@gmail.com>
2026-01-29 21:45:53 +0530

    count even_odd

---
+++ count even_odd.py
@@ -0,0 +1,10 @@
+num = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+even = 0
+odd = 0
+for n in num:
+    if n%2==0:
+        even +=1
+    else:
+        odd +=1 
+print("Total number of Even is:",even)  
+print("Total number of odd is:",odd)       

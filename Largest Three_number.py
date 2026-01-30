24b532144560f3b69f49dfd0affa6638c4734319
Gayanaerappa <gayanaerappa2027@gmail.com>
2026-01-30 22:05:41 +0530

    Largest  3_Number

---
+++ Largest Three_number.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the first number:"))
+b = int(input("Enter the Second number:"))
+c = int(input("Enter the Third number:"))
+if a>b and a>c:
+    print(a,"is the greather")
+elif b>a and b>c:
+    print(b, "is a greather") 
+else:
+    print(c,"is the greather")
+

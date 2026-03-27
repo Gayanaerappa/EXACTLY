8ea928ca44012e674c6a3da9c2e3a9ac9c1256a6
Gayana Erappa <gayanaerappa2027@gmail.com>
2026-03-27 23:00:07 +0530

    Create fun.py

---
+++ fun.py
@@ -0,0 +1,8 @@
+# Simple Python Program
+
+def greet(name):
+    print(f"Hello, {name}! Welcome to GitHub 🚀")
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    greet(user_name)
